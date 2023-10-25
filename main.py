4f94e3be466490cc25136de12e69e4fa9bfb4c7c
Sanjrbek <ssmsan60@gmail.com>
2023-10-25 19:39:03 +0600

    project one

---
+++ main.py
@@ -0,0 +1,317 @@
+
+#while  False: print(eval(input("сан жаз:  ")))
+
+
+#print     
+
+
+
+
+
+
+
+
+
+
+
+#inst 12345678 жонокой сандар   
+#float 9 ,  35545 , 345 болчок сандар 
+
+#str= ("hello")
+ 
+#  x = ("hello") ящик
+#  print (type(x))
+
+
+
+#  #print(type(x))
+#  #print(type(y))
+#  fals жалган
+#  true чындык 
+
+
+#a = "Hello, World!"
+#print(a.upper()) #тамганы чонойтуп берет
+
+
+#a = "HELLO, KANAT"тамганы кичирейтип берет
+#print(a.lower())
+
+
+#a= " hello,kanat"
+
+#print(a.title()) башкы тамганы чонойтуп берет
+
+
+
+#s=[23,45,6,'s',]
+#print (
+
+#print(int(True))
+
+
+
+
+
+#thislist = ["sss",  "vvv", "aaa"]
+#thislist[1] ='eee'
+#print(thislist)
+  
+
+
+#a ="rrrr"
+#b ="eeee"
+#c = a + b
+#print(c)
+
+
+
+
+#a = "ddd", "uuu","uuu","uuu"
+#print(a.count('uuu')) созду эсептейт
+
+
+
+
+#txt = "hello"
+#print(txt.find('l'))
+
+
+
+
+
+
+
+
+
+
+
+# import calendar
+# year = 2023
+# width = 2
+# calendar = calendar.calendar(year, width)
+# print(calendar)                                                       
+
+
+
+
+
+
+
+
+
+
+#list = [0,0,0,0,0,]
+#for  i in  list:
+   
+
+
+
+
+
+
+
+
+#print(i,  'салам ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ssm   =   {
+# 'BRAND' :  'samsung',
+# 'MODEL' :   'A22'   ,
+# 'YEAR'  :   '2020'
+
+
+# }
+# d={}
+# print(ssm.keys())
+#ssm["str"] =333
+
+#ssm . pop('brand')
+
+
+
+
+#x = ssm . items
+
+
+
+
+#ssm["color"]= "red"
+
+
+
+
+
+
+
+
+
+
+
+
+#print(ssm)
+
+
+
+
+
+
+
+#txt = 'salam'
+#save = 'kandaysun'
+#txt,save = save ,txt берилген созду чыгарат
+#rint(txt)
+
+
+
+
+#s = (input('сан жаз ?'))
+#print(s.isdigit())
+
+
+
+
+
+
+
+
+
+# логин  = 'sanjar'
+# password = 3333
+# s =(input('Введите логин:'))
+# print('логин верно')
+# print("пароль верно ")
+#     else:
+#         print("Неверный пороль !")
+# if s <= логин :
+#    print("Введите логин ещё раз")
+#     else :
+#    a == password
+#    print("Попробуйте ещё раз :")
+
+
+  
+
+
+
+
+
+
+
+#for  i in range (1,10):
+#    print(str(i)*i)
+
+
+
+
+
+
+#login  =  'ssm'
+#password  =1111
+#s = (input('login jazz:'))
+#if  s  == login:
+#          print("logine true")
+#else  :
+#          print("logine false")
+#a=int  (input("password  jazz:"))
+
+#if a == password :
+#                   print("password  true")
+#else:
+#                         print('pasword false')
+
+
+
+
+
+
+
+
+
+
+#for i in range ():
+#     print(i)
+
+
+
+
+
+
+
+
+
+
+#for i in  range(1,100):
+#      print(str(i),2)  'esepteyt
+
+
+
+
+
+
+#nomber = 0
+#while nomber < 100:
+ #     nomber +=1
+ #     print(nomber)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#name=input('атыны жаз')
+#name2=list(name)
+#name2.reverse()
+#print(name)
+
+
+
+
+# eee = (input('logine jaze'))
+
+
+# list=[1,]
+# for i in list:
+#   if i ==15:
+#       continue
+#   print(i)
+
+
+
+i =1
+while i <30:
+  i +=1 
+  if i ==25:
+    continue
+print(i)
+
+
+
+
+
+
+
+
+
---
+++ python.py
@@ -0,0 +1,128 @@
+# name = 'sanjar'
+# password = 12345
+# kayra = 'дагы аракет кылып корунуз'
+# a = (input("Введите логин :"))
+# if a == name:
+#     print('туура')
+# else :
+#      print('ката')
+
+# daf =(input('пароль жазыныз:'))
+# if a ==daf:
+#      print('kata')
+# else:
+#      print('tura')
+
+
+
+
+
+
+
+
+
+# set = {0,'aaa','sss','ddd',1,'eeee', True , False,}
+# set3 =an.union(set)кошот
+# print(len(thisset)) esepteyt
+
+
+# print(type(set))type chygarat
+# an.update(set)
+# print(an)кошот
+# for x in set:
+#    print(x)katary menen chygarat
+
+# print('sss'in set)true and false
+
+
+# if 'aaa' in set:
+#      print('bar')
+
+# set.add('ggg')
+# print(set)koshot
+# x = set. pop()
+# print(x)рандом удалить кылат
+# set.remove ("sss")удалить кылат
+# set.discard(0)удалить кылат
+
+
+# for x in range (1, 101,4 ):
+#     print(x)
+
+
+# for x in range(19 ,100,4):
+#     print('салам')
+#     print('sasa')
+
+# a= str(input('соз жаз'))
+# b =int (input('san  jaz'))
+# for i in range (b):
+#     print(a)
+
+
+
+# a=str(input('soz jazz'))
+# b=int(input('san jazz'))
+# for i in range (b):
+#           print(a)
+
+
+
+# a= int (input('san jaz'))
+# for i in  range ( a):
+#     print("@"*100,'$'*1009)
+# print('&'*103)
+
+
+
+# a =int (input('soz jaz'))
+# for i in range(a):
+#    print('san jaz'+ str(i)+ 'raven'+str(i**2))
+
+
+
+# a= int ( input('san jaz'))
+# for i in range(1,10-1):
+#     print(i*'*')
+
+
+# a=int(input('san jaz'))
+# for i in range(1,14,-1):
+#              print(i*'*')
+
+
+
+
+
+
+# z= float (input("san jaz "))
+
+# h=float(input('can jaz'))
+# v=int(input('san jaz'))
+# for i in range(1,z+1):
+  
+#    print(i,float(z))
+#    z = z +z *(h/100)
+
+
+
+# a =int(input('san jaz'))
+# b =int(input('san jaz'))
+# for i in range ( a, b, -2):
+#            print(i)
+
+
+# for a in range(a,b+1):
+#     if a%17 ==0 and a %3==0 or a %17 ==0 or a %10==3:
+#         print(a)
+
+
+
+
+
+# a =int (input('san jaz'))
+# for i in range('1''11'):
+#      print(a, '*',a, '=', i*a )
+
+
+
